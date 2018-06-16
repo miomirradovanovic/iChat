e927566ef44d1da3031a7d82be694148ca75b52c
Миомир Радовановић <miomir.radovanovic@icloud.com>
2018-06-16 18:15:43 +0200

    Initial Commit

---
+++ iChat/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  iChat
+//
+//  Created by Миомир Радовановић on 6/16/18.
+//  Copyright © 2018 Миомир Радовановић. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
