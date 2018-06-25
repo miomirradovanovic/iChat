b33dbd58868297d39c51aa5f559ea889eb68dd91
Миомир Радовановић <miomir.radovanovic@icloud.com>
2018-06-25 12:48:36 +0200

    Up to Lecture 18

--- iChat/FinishRegistrationViewController.swift
+++ iChat/FinishRegistrationViewController.swift
@@ -7,6 +7,7 @@
 //
 
 import UIKit
+import ProgressHUD
 
 class FinishRegistrationViewController: UIViewController {
     
@@ -33,11 +34,87 @@ class FinishRegistrationViewController: UIViewController {
     //MARK: IBActions
     
     @IBAction func cancelButtonPressed(_ sender: Any) {
+        cleanTextFields()
+        dismissKeyboard()
         
+        ProgressHUD.dismiss()
+        
+        self.dismiss(animated: true, completion: nil)
     }
     
     @IBAction func doneButtonPressed(_ sender: Any) {
+        dismissKeyboard()
+        ProgressHUD.show("Registering...")
         
+        if nameTextField.text != "" && surnameTextField.text != "" && countryTextField.text != "" &&
+            cityTextField.text != "" && phoneTextField.text != "" {
+            FUser.registerUserWith(email: email!, password: password!, firstName: nameTextField.text!, lastName: surnameTextField.text!) { (error) in
+                if error != nil {
+                    ProgressHUD.dismiss()
+                    ProgressHUD.showError(error!.localizedDescription)
+                    return
+                }
+                self.registerUser()
+            }
+        } else {
+            ProgressHUD.showError("All fields are required!")
+        }
+    }
+    
+    
+    //MARK: Helpers
+    
+    func registerUser() {
+        let fullName = nameTextField.text! + " " + surnameTextField.text!
+        
+        var tempDictionary : Dictionary = [kFIRSTNAME : nameTextField.text!,
+                                           kLASTNAME : surnameTextField.text!,
+                                           kFULLNAME : fullName,
+                                           kCOUNTRY : countryTextField.text!,
+                                           kCITY : cityTextField.text!,
+                                           kPHONE : phoneTextField.text!]
+            as [String : Any]
+        
+        if avatarImage == nil {
+            imageFromInitials(firstName: nameTextField.text!, lastName: surnameTextField.text!) { (avatarInitials) in
+                let avatarIMG = avatarInitials.jpegData(compressionQuality: 0.7)
+                let avatar = avatarIMG!.base64EncodedString(options: NSData.Base64DecodingOptions(rawValue: 0))
+                
+                tempDictionary[kAVATAR] = avatar
+                self.finishRegistration(withValues: tempDictionary)
+            }
+        } else {
+//            let avatarData = avatarImage.jpegData(compressionQuality: 0.7)
+//            let avatar = avatarData!.base64EncodedString(options: NSData.Base64DecodingOptions(rawValue: 0))
+            
+//            tempDictionary[kAVATAR] = avatar
+            self.finishRegistration(withValues: tempDictionary)
+        }
+    }
+    
+    func finishRegistration(withValues: [String : Any]) {
+        updateCurrentUserInFirestore(withValues: withValues) { (error) in
+            if error != nil {
+                DispatchQueue.main.async {
+                    ProgressHUD.showError(error!.localizedDescription)
+                }
+                return
+            }
+            
+            // go to app
+        }
+    }
+    
+    func dismissKeyboard() {
+        self.view.endEditing(false)
+    }
+    
+    func cleanTextFields() {
+        nameTextField.text = ""
+        surnameTextField.text = ""
+        countryTextField.text = ""
+        cityTextField.text = ""
+        phoneTextField.text = ""
     }
     
 }
